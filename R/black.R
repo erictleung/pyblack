cc7e77cdd2839413fe60f03d370cadd575d2f0f9
Eric Leung <eric@erictleung.com>
2023-05-27 17:52:41 -0700

    Condition on whether pyproject.toml exists to use

--- R/black.R
+++ R/black.R
@@ -21,15 +21,24 @@ style_black <- function() {
     extract2(1) %>%
     extract2("text")
 
-  # Run black on temporary file
+  # Write code into temporary file
   tmpFile <- tempfile(fileext = ".py")
   writeLines(code, tmpFile)
-  black_config <- normalizePath(file.path(getwd(), "pyproject.toml"))
+
+  # Setup use of local configuration for black
+  black_config_path <- file.path(getwd(), "pyproject.toml")
+  black_config <- ifelse(
+    file.exists(black_config_path),
+    paste("--config", normalizePath(black_config_path), sep = " "),
+    ""
+  )
+
+  # Run black on code
   x <- suppressWarnings(system2(
     "black",
     c(
       "-v",  # Return more verbose stdout and stderr, used for troubleshooting
-      paste("--config", black_config, sep = " "),  # Use working dir config
+      black_config,  # Use config if it exists in the working directory
       tmpFile
     ),
     stdout = TRUE,
